0611c8f9fbfa42bf1f330863a3bafb1c18396ece
Vitalij Ryndin <sys@crashmax.ru>
2021-10-07 16:33:29 +0800

    update webpack to v5

---
+++ webpack/webpack.common.js
@@ -0,0 +1,58 @@
+const { DefinePlugin } = require('webpack')
+const { paths, version } = require('./webpack.config')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+/** @type {import('webpack').Configuration} */
+module.exports = {
+  entry: paths.entry,
+  output: {
+    path: paths.output,
+    filename: '[name].js',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true
+  },
+  externals: {
+    './fireworks': './fireworks',
+    react: 'react'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx$/,
+        loader: 'ts-loader'
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      esmodules: true
+                    }
+                  }
+                ]
+              ]
+            }
+          },
+          {
+            loader: 'ts-loader'
+          }
+        ]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new DefinePlugin({ version })
+  ]
+}
