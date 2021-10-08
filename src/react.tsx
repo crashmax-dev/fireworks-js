b5065950f5627b0c42d7492c4fcabdc6b5cd30f8
Vitalij Ryndin <sys@crashmax.ru>
2021-10-08 18:12:50 +0800

    feat(test): react class components & parcel serve

--- src/react.tsx
+++ src/react.tsx
@@ -1,32 +1,32 @@
-import React, { CSSProperties, useEffect, useRef } from 'react'
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+import React, { Component, CSSProperties } from 'react'
 import { FireworksOptions, Fireworks as Fw } from './fireworks'
 
-interface FireworksProps {
+export interface FireworksProps {
   options?: FireworksOptions
   style?: CSSProperties
 }
 
-const Fireworks: React.FC<FireworksProps> = ({
-  options,
-  style,
-  children
-}) => {
-  const canvas = useRef() as React.MutableRefObject<HTMLDivElement>
+export class Fireworks extends Component<FireworksProps> {
+  _fw: Fw
+  _ref: HTMLElement | null = null
 
-  useEffect(() => {
-    const fireworks = new Fw(canvas.current, options)
-
-    fireworks.setSize()
-    fireworks.start()
-
-    return () => {
-      fireworks.stop()
+  componentDidMount() {
+    if (this._ref) {
+      this._fw = new Fw(this._ref, this.props.options)
+      this._fw.start()
     }
-  }, [])
+  }
 
-  return (
-    <div style={style} ref={canvas}>{children}</div>
-  )
+  componentWillUnmount() {
+    this._fw.stop()
+  }
+
+  render() {
+    return React.createElement('div', {
+      ref: (ref: HTMLElement) => (this._ref = ref),
+      children: this.props.children,
+      style: this.props.style
+    })
+  }
 }
-
-export { Fireworks, FireworksOptions }
