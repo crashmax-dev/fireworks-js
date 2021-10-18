cfe9620304ecbb60989eac81b1732b2f867f0576
Vitalij Ryndin <sys@crashmax.ru>
2021-10-19 01:12:47 +0800

    chore: add react example

---
+++ examples/react.tsx
@@ -0,0 +1,37 @@
+import React, { useState, CSSProperties } from 'react'
+import { Fireworks } from '../src/react'
+import { FireworksOptions } from '../src/fireworks'
+
+export const App: React.FC = () => {
+  const [isEnable, setEnable] = useState(true)
+
+  const options: FireworksOptions = {
+    speed: 3,
+    mouse: {
+      max: 3,
+      move: false,
+      click: true
+    }
+  }
+
+  const style: CSSProperties = {
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    position: 'fixed',
+    background: '#000'
+  }
+
+  return (
+    <div>
+      <button
+        onClick={() => setEnable(!isEnable)}
+        style={{ position: 'absolute', zIndex: 9999 }}
+      >
+        {isEnable ? 'Stop' : 'Start'}
+      </button>
+      {isEnable && <Fireworks options={options} style={style} />}
+    </div>
+  )
+}
