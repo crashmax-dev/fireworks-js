2a57ebf4043fbc685a67e6b308f7d82f7ac77b2a
crashmax-dev <crashmax-dev@users.noreply.github.com>
2022-03-19 08:20:00 +0000

    Deploying to gh-pages from @ crashmax-dev/fireworks-js@e0f361718d8d5dd9938e0239ad0685d1271933ea 🚀

--- helpers.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function randomFloat(min: number, max: number): number;
-export declare function randomInt(min: number, max: number): number;
-export declare function getDistance(x: number, y: number, dx: number, dy: number): number;
-export declare function hsla(hue: number, brightness: number, alpha?: number): string;
