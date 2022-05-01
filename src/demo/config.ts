86a0a1bd663efd4911e8678ac125730f1447d279
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-05-01 18:20:29 +0800

    refactor demo website

---
+++ src/demo/config.ts
@@ -0,0 +1,86 @@
+import type { FireworksOptions } from '../fireworks'
+
+export const mainContainer = document
+  .querySelector<HTMLDivElement>('.container')!
+
+export const fireworksContainer = document
+  .querySelector<HTMLDivElement>('.fireworks-container')!
+
+export const fireworksOptions: FireworksOptions = {
+  hue: {
+    min: 0,
+    max: 345
+  },
+  delay: {
+    min: 30,
+    max: 60
+  },
+  rocketsPoint: {
+    min: 50,
+    max: 50
+  },
+  opacity: 0.5, // fillStyle
+  acceleration: 1.02,
+  friction: 0.97,
+  gravity: 1.5,
+  particles: 90,
+  trace: 3,
+  traceSpeed: 10,
+  explosion: 6,
+  intensity: 30,
+  flickering: 50,
+  lineStyle: 'round',
+  lineWidth: {
+    explosion: {
+      min: 1,
+      max: 4
+    },
+    trace: {
+      min: 0.1,
+      max: 1
+    }
+  },
+  autoresize: true,
+  brightness: {
+    min: 50,
+    max: 80,
+    decay: {
+      min: 0.015,
+      max: 0.03
+    }
+  },
+  boundaries: {
+    x: 50,
+    y: 50,
+    width: fireworksContainer.clientWidth,
+    height: fireworksContainer.clientHeight,
+    visible: false
+  },
+  sound: {
+    enabled: false,
+    files: [
+      location.href + 'sounds/explosion0.mp3',
+      location.href + 'sounds/explosion1.mp3',
+      location.href + 'sounds/explosion2.mp3'
+    ],
+    volume: {
+      min: 2,
+      max: 4
+    }
+  },
+  mouse: {
+    click: true,
+    move: false,
+    max: 3
+  }
+}
+
+export const backgroundConfig = {
+  color: '#000000',
+  image: '',
+  size: 'cover',
+  position: '50% 50%',
+  repeat: 'no-repeat',
+  container: false,
+  fps: false
+}
--- src/demo/index.ts
+++ src/demo/index.ts
@@ -1,89 +1,8 @@
 import { Pane } from 'tweakpane'
 import * as EssentialsPlugin from '@tweakpane/plugin-essentials'
 import { Fireworks } from '../fireworks'
-import type { FireworksOptions } from '../fireworks'
+import { fireworksContainer, fireworksOptions, backgroundConfig, mainContainer } from './config'
 import type { FpsGraphBladeApi } from '@tweakpane/plugin-essentials/dist/types/fps-graph/api/fps-graph'
-
-const container = document.querySelector<HTMLDivElement>('.container')!
-const fireworksContainer = document.querySelector<HTMLDivElement>('.fireworks-container')!
-const fireworksOptions: FireworksOptions = {
-  hue: {
-    min: 0,
-    max: 345
-  },
-  delay: {
-    min: 30,
-    max: 60
-  },
-  rocketsPoint: {
-    min: 50,
-    max: 50
-  },
-  opacity: 0.5, // fillStyle
-  acceleration: 1.02,
-  friction: 0.97,
-  gravity: 1.5,
-  particles: 90,
-  trace: 3,
-  traceSpeed: 10,
-  explosion: 6,
-  intensity: 30,
-  flickering: 50,
-  lineStyle: 'round',
-  lineWidth: {
-    explosion: {
-      min: 1,
-      max: 4
-    },
-    trace: {
-      min: 0.1,
-      max: 1
-    }
-  },
-  autoresize: true,
-  brightness: {
-    min: 50,
-    max: 80,
-    decay: {
-      min: 0.015,
-      max: 0.03
-    }
-  },
-  boundaries: {
-    x: 50,
-    y: 50,
-    width: fireworksContainer.clientWidth,
-    height: fireworksContainer.clientHeight,
-    visible: false
-  },
-  sound: {
-    enabled: false,
-    files: [
-      location.href + 'sounds/explosion0.mp3',
-      location.href + 'sounds/explosion1.mp3',
-      location.href + 'sounds/explosion2.mp3'
-    ],
-    volume: {
-      min: 2,
-      max: 4
-    }
-  },
-  mouse: {
-    click: true,
-    move: false,
-    max: 10
-  }
-}
-
-const backgroundConfig = {
-  color: '#000000',
-  image: '',
-  size: 'cover',
-  position: '50% 50%',
-  repeat: 'no-repeat',
-  container: false,
-  fps: false
-}
 
 const fireworks = new Fireworks(fireworksContainer, fireworksOptions)
 fireworks.start()
@@ -246,7 +165,9 @@ const background = tweakpane.addFolder({
 background
   .addInput(backgroundConfig, 'container')
   .on('change', ({ value }) => {
-    container.style.display = value ? 'none' : 'block'
+    mainContainer.style.display = value ?
+      'none' :
+      'block'
   })
 
 background
