d765e1c8fb74b56b142564a1034206386198fe5a
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-07-04 17:16:23 +0800

    publish `@fireworks-js/helpers@0.0.0-beta.1`

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -16,18 +16,18 @@ export function getDistance(
   return Math.sqrt(pow(x - dx, 2) + pow(y - dy, 2))
 }
 
-export function hsla(hue: number, brightness: number, alpha = 1): string {
-  if (hue > 255 || hue < 0) {
-    throw new Error(`Expected hue 0-255 range, got \`${hue}\``)
+export function hsla(hue: number, lightness: number, alpha = 1): string {
+  if (hue > 360 || hue < 0) {
+    throw new Error(`Expected hue 0-360 range, got \`${hue}\``)
   }
 
-  if (brightness > 100 || brightness < 0) {
-    throw new Error(`Expected brightness 0-100 range, got \`${brightness}\``)
+  if (lightness > 100 || lightness < 0) {
+    throw new Error(`Expected lightness 0-100 range, got \`${lightness}\``)
   }
 
   if (alpha > 1 || alpha < 0) {
     throw new Error(`Expected alpha 0-1 range, got \`${alpha}\``)
   }
 
-  return `hsla(${hue}, 100%, ${brightness}%, ${alpha})`
+  return `hsla(${hue}, 100%, ${lightness}%, ${alpha})`
 }
--- packages/helpers/test/hsla.test.ts
+++ packages/helpers/test/hsla.test.ts
@@ -22,7 +22,7 @@ test('hsla', (t) => {
     hsla(0, 0, -1)
   }, instanceError)
 
-  t.is(throwHue!.message, 'Expected hue 0-255 range, got `-1`')
-  t.is(throwBrightness!.message, 'Expected brightness 0-100 range, got `-1`')
+  t.is(throwHue!.message, 'Expected hue 0-360 range, got `-1`')
+  t.is(throwBrightness!.message, 'Expected lightness 0-100 range, got `-1`')
   t.is(throwAlpha!.message, 'Expected alpha 0-1 range, got `-1`')
 })
