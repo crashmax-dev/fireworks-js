dd6932b9b72765c0d11b4a6ca1ac6e65cd4f454a
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-07-07 00:25:12 +0800

    add vue example

---
+++ examples/with-vue/fireworks.vue
@@ -0,0 +1,17 @@
+<template>
+  <Fireworks
+    :options="{ mouse: { click: true } }"
+    :style="{
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      position: 'fixed',
+      background: '#000'
+    }"
+  />
+</template>
+
+<script setup>
+import { Fireworks } from '@fireworks-js/vue'
+</script>
---
+++ examples/with-vue/index.js
@@ -0,0 +1,4 @@
+import { createApp } from 'vue'
+import App from './fireworks.vue'
+
+createApp(App).mount('#app')
