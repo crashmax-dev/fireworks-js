0720d8318a3e51bb401699f25367153c0d37d9ed
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-07-06 19:51:19 +0800

    add example `with-react`

---
+++ examples/with-react/index.tsx
@@ -0,0 +1,32 @@
+import React from 'react' // React is not defined
+import { CSSProperties, useState } from 'react'
+import { createRoot } from 'react-dom/client'
+import Fireworks from '@fireworks-js/react'
+
+const app = document.querySelector('#app')!
+const root = createRoot(app)
+const style: CSSProperties = {
+  top: 0,
+  left: 0,
+  width: '100%',
+  height: '100%',
+  position: 'fixed',
+  background: '#000'
+}
+
+function App() {
+  const [enabled, setEnabled] = useState(true)
+  return (
+    <>
+      <button
+        onClick={() => setEnabled(!enabled)}
+        style={{ position: 'absolute', zIndex: 1 }}
+      >
+        {enabled ? 'Disable' : 'Enable'}
+      </button>
+      {enabled && <Fireworks style={style} />}
+    </>
+  )
+}
+
+root.render(<App />)
