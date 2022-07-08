f27a98cbd4a80290e06ca160831a5c7ca1bf828d
Vitalij Ryndin <crashmax.dev@gmail.com>
2022-07-08 23:43:39 +0800

    chore(example): update react

--- examples/with-react/src/index.tsx
+++ examples/with-react/src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
-const app = document.getElementById('root')!
+const app = document.querySelector('#app')!
 createRoot(app).render(<App />)
